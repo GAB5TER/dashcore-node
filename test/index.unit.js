c241de0925fbbe55a858b22817356d1824bab2eb
GAB5TER <GAB5TER@protonmail.com>
2018-12-01 01:20:13 +0000

    Updated remaining unit tests

--- test/index.unit.js
+++ test/index.unit.js
@@ -3,10 +3,10 @@
 var should = require('chai').should();
 
 describe('Index Exports', function() {
-  it('will export dashcore-lib', function() {
-    var dashcore = require('../');
-    should.exist(dashcore.lib);
-    should.exist(dashcore.lib.Transaction);
-    should.exist(dashcore.lib.Block);
+  it('will export orecore-lib', function() {
+    var orecore = require('../');
+    should.exist(orecore.lib);
+    should.exist(orecore.lib.Transaction);
+    should.exist(orecore.lib.Block);
   });
 });
--- test/node.unit.js
+++ test/node.unit.js
@@ -2,15 +2,15 @@
 
 var should = require('chai').should();
 var sinon = require('sinon');
-var dashcore = require('@dashevo/dashcore-lib');
-var Networks = dashcore.Networks;
+var orecore = require('orecore-lib');
+var Networks = orecore.Networks;
 var proxyquire = require('proxyquire');
 var util = require('util');
 var BaseService = require('../lib/service');
 var index = require('../lib');
 var log = index.log;
 
-describe('Dashcore Node', function() {
+describe('Orecore Node', function() {
 
   var baseConfig = {};
 
@@ -425,7 +425,7 @@ describe('Dashcore Node', function() {
 
   describe('#getNetworkName', function() {
     afterEach(function() {
-      dashcore.Networks.disableRegtest();
+      orecore.Networks.disableRegtest();
     });
     it('it will return the network name for livenet', function() {
       var node = new Node(baseConfig);
